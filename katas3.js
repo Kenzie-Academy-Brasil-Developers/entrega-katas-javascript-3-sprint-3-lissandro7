25547f017a0e638163147611d53f15073972008c
lissandro <lissandroperf7@gmail.com>
2021-09-06 16:20:10 -0300

    Enviando Tarefa Concluida

--- katas3.js
+++ katas3.js
@@ -1,76 +1,241 @@
 const sampleArray = [469, 755, 244, 245, 758, 450, 302, 20, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
 
-function kata1() {
-    // implemente o código do kata 1 aqui
+
+
+
+function showResults(tit, parametro) {
+    const corpo = document.getElementById('teste');
+    const menu = document.createElement('div');
+    const adc = document.createElement('p');
+    const titulo = document.createElement("h3");
+
+    titulo.innerText = tit;
+    menu.appendChild(titulo);
+
+    corpo.appendChild(menu);
+    adc.innerText = "(" + parametro + ")";
+    menu.appendChild(adc);
 }
+
+
+function kata1() {
+    let guardar = [];
+    for (let c = 1; c <= 25; c++) {
+
+        guardar.push(" " + c + " ");
+    }
+    return showResults(" Numeros de 1 a 25", guardar)
+}
+
+kata1();
 
 function kata2() {
-    // implemente o código do kata 2 aqui
+    let guardar = [];
+    for (let c = 25; c >= 1; c--) {
+
+        guardar.push(" " + c + " ");
+    }
+    return showResults(" Numeros de 1 a 25", guardar)
 }
+
+kata2();
 
 function kata3() {
-    // implemente o código do kata 3 aqui
+
+    let guarda = [];
+    for (let c = -1; c >= -25; c--) {
+
+        guarda.push(" " + c + " ")
+    }
+    return showResults("Numeros -1 a -25", guarda)
 }
+
+kata3();
 
 function kata4() {
-    // implemente o código do kata 4 aqui
+    let guardar = [];
+    for (let c = -25; c <= -1; c++) {
+
+        guardar.push(" " + c + " ")
+    }
+    return showResults("Numeros -25 a -1 ", guardar)
 }
+kata4();
 
 function kata5() {
-    // implemente o código do kata 5 aqui
+    let guardar = [];
+
+    for (let c = 25; c >= -25; c--) {
+        if (c % 2 == 1 || c % -2 == -1) {
+            guardar.push(" " + c + " ")
+        }
+    }
+    return showResults("Numeros impares de 25 a -25", guardar)
 }
+kata5();
 
 function kata6() {
-    // implemente o código do kata 6 aqui
+    let guardar = [];
+
+    for (let c = 3; c <= 100; c++) {
+        if (c % 3 == 0) {
+            guardar.push(" " + c + " ")
+        }
+    }
+    return showResults("Numeros Divisiveis por 3. de 3 ate o 100", guardar)
 }
+
+kata6();
 
 function kata7() {
-    // implemente o código do kata 7 aqui
-}
+    let guardar = [];
 
-function kata8() {
-    // implemente o código do kata 8 aqui
+    for (let c = 7; c <= 100; c++) {
+        if (c % 7 == 0) {
+            guardar.push(" " + c + " ");
+        }
+    }
+    return showResults("Numeros divisiveis por 7. de 7 ate o 100", guardar)
 }
+kata7();
+function kata8() {
+    let guardar = []
+
+    for (let c = 100; c >= 1; c--) {
+        if (c % 3 == 0 || c % 7 == 0) {
+            guardar.push(" " + c + " ")
+        }
+    }
+    return showResults("Numeros divisiveis por 3 e 7 apartir de 100", guardar)
+}
+kata8();
 
 function kata9() {
-    // implemente o código do kata 9 aqui
+    let guardar = [];
+
+    for (let c = 1; c <= 100; c++) {
+        if (c % 2 == 1 && c % 5 === 0) {
+            guardar.push(" " + c + " ")
+        }
+    }
+    return showResults("Numeros impares e divisiveis por 5 ", guardar)
 }
+kata9();
 
 function kata10() {
-    // implemente o código do kata 10 aqui
+
+    let opa = [];
+    for (let c = 0; c <= sampleArray.length; c++) {
+
+        opa.push(sampleArray[c])
+    }
+
+    return showResults("Os 20 elementos do array", opa)
+
+
 }
+kata10();
 
 function kata11() {
-    // implemente o código do kata 11 aqui
+
+    let opa = [];
+    for (let c = 0; c <= sampleArray.length; c++) {
+        if (sampleArray[c] % 2 == 0) {
+            opa.push(" " + sampleArray[c] + " ")
+        }
+    }
+    return showResults("Numeros pares do array ", opa)
 }
+kata11();
 
 function kata12() {
-    // implemente o código do kata 12 aqui
+
+    let opa = [];
+    for (let c = 0; c <= sampleArray.length; c++) {
+        if (sampleArray[c] % 2 == 1) {
+
+            opa.push(" " + sampleArray[c] + " ")
+        }
+    }
+    return showResults("Numeros impares do array", opa)
 }
+kata12();
 
 function kata13() {
-    // implemente o código do kata 13 aqui
+
+    let opa = [];
+    for (let c = 0; c <= sampleArray.length; c++) {
+        if (sampleArray[c] % 8 == 0) {
+
+            opa.push(" " + sampleArray[c] + " ")
+        }
+    }
+    return showResults("Numeros divisiveis por 8 no array", opa)
 }
+kata13();
 
 function kata14() {
-    // implemente o código do kata 14 aqui
+
+    let armazenar = []
+    for (let c = 0; c <= sampleArray[c]; c++) {
+
+        let quadrado = sampleArray[c] * sampleArray[c];
+        armazenar.push(" " + quadrado + " ")
+    }
+
+    return showResults("Numero quadrado do array", armazenar)
 }
+kata14();
 
 function kata15() {
-    // implemente o código do kata 15 aqui
+
+    let soma = 0;
+    for (let c = 0; c <= 20; c++) {
+        soma += c;
+    }
+    return showResults("Soma dos Numeros de 1 a 20", soma)
 }
+kata15();
 
 function kata16() {
-    // implemente o código do kata 16 aqui
+
+    let soma = [];
+    for (let c = 0; c <= sampleArray.length; c++) {
+
+        soma += c;
+    }
+    return showResults("Soma so Array", soma)
 }
+kata16();
+
 
 function kata17() {
-    // implemente o código do kata 17 aqui
+
+    let menos = sampleArray[0];
+    for (let c = 0; c < sampleArray.length; c++) {
+        if (sampleArray[c] < menos) {
+            menos = sampleArray[c];
+
+        }
+    }
+    return showResults("Menor Numero do Array", menos);
 }
 
+kata17();
+
 function kata18() {
-    // implemente o código do kata 18 aqui
+    
+    let mais = sampleArray[0];
+    for (let c = 0; c < sampleArray.length; c++) {
+        if (sampleArray[c] > mais) {
+            mais = sampleArray[c]
+        }
+    }
+
+    return showResults("Maior Numnero do Array", mais)
 }
+
+kata18();
 
 /**
  * Daqui em diante são os bônus, por sua conta e risco
